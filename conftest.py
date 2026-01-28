348c2bf88bc7de13b4f910a10af208eaed7b8795
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-28 19:57:55 +0200

    update conftest(add create user)

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
+import random
+import requests
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def api_client():
     token = "8a9e0b0229586c423143abefaa395d29f03db9a20fb5ccd70262a45053b54467"
     headers = {
@@ -12,3 +14,20 @@ def api_client():
     base_url = "https://gorest.co.in/public/v2"
 
     return headers, base_url
+
+@pytest.fixture(scope='session')
+def test_create_new_user(api_client):
+    test_number = random.randint(0, 999)
+    headers, base_url = api_client
+    url = f"{base_url}/users"
+    new_user = {
+        "name":"Mezhen",
+        "gender":"male",
+        "email":f"testemail{test_number}@gmail.com",
+        "status":"active"
+    }
+    response = requests.post(url, json=new_user, headers=headers)
+    assert response.ok
+    result = response.json()
+    assert len(result) > 0
+    return result["id"]
