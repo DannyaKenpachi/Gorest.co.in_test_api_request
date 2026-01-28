865c10afbf995ebe491ec52e1355996e8368f5f0
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-28 20:01:36 +0200

    update test_api(add allure)

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,27 +3,36 @@ import allure
 import requests
 import random
 
+@allure.feature("Проверка API")
+@allure.title("Проверка списка пользователей")
 def test_list_users(api_client):
-    headers, base_url = api_client
-    url = f"{base_url}/users"
-    response = requests.get(url, headers=headers)
-    assert response.ok
-    result = response.json()
-    assert len(result) > 0
+    with allure.step("Подготовка данных"):
+        headers, base_url = api_client
+        url = f"{base_url}/users"
+    with allure.step("Отправка запроса"):    
+        response = requests.get(url, headers=headers)
+    with allure.step("Проверка ответа"):  
+        assert response.ok
+        result = response.json()
+        assert len(result) > 0
 
+@allure.title("Проверка обновление данных о пользователе")
 def test_update_user(api_client, test_create_new_user):
-    test_number = random.randint(0, 999)
-    headers, base_url = api_client
-    url = f"{base_url}/users/{test_create_new_user}"
-    new_user = {
-        "name":"Nezhen",
-        "gender":"male",
-        "email":f"testemail{test_number}@gmail.com",
-        "status":"active"
-    }
-    response = requests.patch(url, json=new_user, headers=headers)
-    assert response.ok
-    result = response.json()
-    print(result)
-    assert len(result) > 0
+    with allure.step("Подготовка данных"):
+        test_number = random.randint(0, 999)
+        headers, base_url = api_client
+        url = f"{base_url}/users/{test_create_new_user}"
+        new_user = {
+            "name":"Nezhen",
+            "gender":"male",
+            "email":f"testemail{test_number}@gmail.com",
+            "status":"active"
+        }
+    with allure.step("Отправка запроса"):
+        response = requests.patch(url, json=new_user, headers=headers)
+    with allure.step("Проверка ответа"): 
+        assert response.ok
+        result = response.json()
+        print(result)
+        assert len(result) > 0
 
